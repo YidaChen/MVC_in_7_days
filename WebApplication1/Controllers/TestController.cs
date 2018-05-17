61f0097908ce231915f79d5fb255ab90a815eb75
Yida Chen <yida820310@gmail.com>
2018-05-17 20:17:36 +0800

    Day 1

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address1";
+            return c;
+        }
+        [NonAction]
+        public string SimpleMethod()
+        {
+            return "Hi, I am not action method";
+        }
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+}
