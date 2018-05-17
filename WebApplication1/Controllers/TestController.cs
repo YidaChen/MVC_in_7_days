1c698aac11bf2a28db2bbff9b3aab062a4c6f8f9
Yida Chen <yida820310@gmail.com>
2018-05-17 20:34:17 +0800

    D2 Lab 6 – Implementing View Model

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -40,7 +41,21 @@ namespace WebApplication1.Controllers
             emp.FirstName = "Sukesh";
             emp.LastName = "Marla";
             emp.Salary = 20000;
-            return View("MyView", emp);
+
+            EmployeeViewModel vmEmp = new EmployeeViewModel();
+            vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            vmEmp.Salary = emp.Salary.ToString("C");
+            if (emp.Salary > 15000)
+            {
+                vmEmp.SalaryColor = "yellow";
+            }
+            else
+            {
+                vmEmp.SalaryColor = "green";
+            }
+            vmEmp.UserName = "Admin";
+
+            return View("MyView", vmEmp);
         }
     }
 }
