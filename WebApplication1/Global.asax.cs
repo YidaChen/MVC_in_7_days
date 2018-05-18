93d9ebe7db1fc3e76da09dad37d84b6068ec195c
Yida Chen <yida820310@gmail.com>
2018-05-18 11:06:42 +0800

    D3 Lab 13 – Add Server side Validation

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApplication1.DataAccessLayer;
 
 namespace WebApplication1
 {
@@ -16,6 +18,8 @@ namespace WebApplication1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //model改變後, 把整個資料庫刪掉 (code first)
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SalesERPDAL>());
         }
     }
 }
