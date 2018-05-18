439da7bd17dddb02de42b5d3d6099b34bc8b80ad
Yida Chen <yida820310@gmail.com>
2018-05-18 21:52:16 +0800

    D5 Lab 25 – Implement Consistent look across project

--- WebApplication1/ViewModels/EmployeeListViewModel.cs
+++ WebApplication1/ViewModels/EmployeeListViewModel.cs
@@ -5,10 +5,8 @@ using System.Web;
 
 namespace WebApplication1.ViewModels
 {
-    public class EmployeeListViewModel
+    public class EmployeeListViewModel : BaseViewModel
     {
         public List<EmployeeViewModel> Employees { get; set; }
-        public string UserName { get; set; }
-        public FooterViewModel FooterData { get; set; }
     }
 }
