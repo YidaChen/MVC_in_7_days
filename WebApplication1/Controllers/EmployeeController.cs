ce225f424fe65f23aebf5a2fdf3b6bec72e242d1
Yida Chen <yida820310@gmail.com>
2018-05-18 16:44:16 +0800

    D5 Lab 22 - Add Footer

--- WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -61,6 +61,9 @@ namespace WebApplication1.Controllers
                 empViewModels.Add(empViewModel);
             }
             employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.FooterData = new FooterViewModel();
+            employeeListViewModel.FooterData.CompanyName = "StepByStepSchools";//Can be set to dynamic value
+            employeeListViewModel.FooterData.Year = DateTime.Now.Year.ToString();
             return View("Index", employeeListViewModel);
         }
         public ActionResult AddNew()
