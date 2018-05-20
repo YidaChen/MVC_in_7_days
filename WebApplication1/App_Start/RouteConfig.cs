2ce18df96093125b7ab3ed9e6d60673a4aa66f9c
Yida Chen <yida820310@gmail.com>
2018-05-20 14:28:37 +0800

    D6 Lab 31 – Implement User friendly URLs

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace WebApplication1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                name: "Upload",
+                url: "Employee/BulkUpload",
+                defaults: new { controller = "BulkUpload", action = "Index" }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
